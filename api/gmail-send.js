67789b34df74a8a347ce21f0fe626022479fdb91
gavinellispro-cyber <gavin.ellis.pro@gmail.com>
2026-06-23 16:00:30 +0700

    Update gmail-send.js

--- api/gmail-send.js
+++ api/gmail-send.js
@@ -51,7 +51,7 @@ function makeEmail({ to, from, subject, body, threadId, replyToMessageId }) {
     .replace(/=+$/, '');
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
